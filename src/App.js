77d91fba9485f8da6570a64416661768d65bc235
RobertBehm <59755088+RobertBehm@users.noreply.github.com>
2020-10-10 22:38:49 -0400

    added travis file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Im a changed app and i like it</p>
+        <p>Am I still working</p>
         <a
           className="App-link"
           href="https://reactjs.org"
